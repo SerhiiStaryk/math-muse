21007c680aa2a4d4730dbe94e826b1f6d776ecd3
Serhii Staryk <serhii.staryk@gmail.com>
2025-11-30 16:15:36 +0200

    fix render issue

--- src/components/GameSection/index.tsx
+++ src/components/GameSection/index.tsx
@@ -1,15 +1,13 @@
 import { Box, Grid } from '@mui/material';
 import type { GameStats } from '@/types';
 import { loadResults } from '@/helpers';
-import { useEffect, useState } from 'react';
+import { useMemo } from 'react';
 import { GAME_CARDS } from './constants';
 import { GameSectionTitle } from './GameSectionTitle';
 import { GameCard } from './GameCard';
 
 export const GameSection = () => {
-  const [gameStats, setGameStats] = useState<GameStats>({});
-
-  useEffect(() => {
+  const gameStats = useMemo(() => {
     const stats: GameStats = {};
 
     GAME_CARDS.forEach(game => {
@@ -17,18 +15,17 @@ export const GameSection = () => {
         const results = loadResults(game.gameType);
         const resultsArray = Object.values(results);
         const total = resultsArray.length;
-
         const mastered = resultsArray.filter(r => r.correct >= 5).length;
 
         stats[game.gameType] = { total, mastered };
       }
     });
 
-    setGameStats(stats);
+    return stats;
   }, []);
 
   return (
-    <Box sx={{ mb: 5 }}>
+    <Box mb={5}>
       <GameSectionTitle />
       <Grid
         container
