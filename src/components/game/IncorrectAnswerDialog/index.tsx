94e934f3d67dcc241ad9acb307d1706fe7b88868
Serhii Staryk <serhii.staryk@gmail.com>
2026-02-16 23:50:45 +0200

    bug: fix bug related to modal window

--- src/components/game/IncorrectAnswerDialog/index.tsx
+++ src/components/game/IncorrectAnswerDialog/index.tsx
@@ -5,7 +5,7 @@ import {
   DialogContent,
   DialogActions,
   Button,
-  DialogContentText,
+  Box,
 } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 
@@ -25,11 +25,16 @@ export const IncorrectAnswerDialog = ({ open, correctAnswer, onClose }: Incorrec
     >
       <DialogTitle>{t('game.tryAgain')}</DialogTitle>
       <DialogContent>
-        <DialogContentText sx={{ textAlign: 'center' }}>
-          {t('game.correctAnswerWas')}
-          <Typography variant='h2'>{correctAnswer}</Typography>
-          {t('game.keepPracticing')}
-        </DialogContentText>
+        <Box sx={{ textAlign: 'center', py: 2 }}>
+          <Typography variant='body1'>{t('game.correctAnswerWas')}</Typography>
+          <Typography
+            variant='h2'
+            sx={{ my: 2, fontWeight: 'bold', color: 'primary.main' }}
+          >
+            {correctAnswer}
+          </Typography>
+          <Typography variant='body1'>{t('game.keepPracticing')}</Typography>
+        </Box>
       </DialogContent>
       <DialogActions sx={{ justifyContent: 'center' }}>
         <Button
